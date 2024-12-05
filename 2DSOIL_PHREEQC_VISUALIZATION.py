07ffa1ce9c9c5025f97191a0032422ef3fecbec0
adityain2003@gmail.com <adityain2003@gmail.com>
2024-12-05 14:03:10 -0500

    PLACED ARRAYS CLOSER TO IMAGING SECTION OF THE CODE

--- 2DSOIL_PHREEQC_VISUALIZATION.py
+++ 2DSOIL_PHREEQC_VISUALIZATION.py
@@ -32,34 +32,23 @@ print("NUM_DAYS =", NUM_DAYS)
 X_ARRAY = (FIELDS_PD[["X"]].to_numpy())[0:NUM_NODES]
 Y_ARRAY = (FIELDS_PD[["Y"]].to_numpy())[0:NUM_NODES]
 
-Q_ARRAY = FIELDS_PD[["thNew"]].to_numpy()
-Q_ARRAY_DATE = NP.zeros(NUM_NODES,dtype=float)
-
-#for J in range(0,NUM_DAYS):
-
-J = 153
-#for J in range(0,NUM_DAYS):
-for I in range(0,NUM_NODES):
-    Q_ARRAY_DATE[I] = Q_ARRAY[I+J*NUM_NODES]
-#    os.system("pause")
-        
+THETA_ARRAY = FIELDS_PD[["thNew"]].to_numpy()
+THETA_ARRAY_DATE = NP.zeros(NUM_NODES,dtype=float)     
 
 for I in range(0,NUM_NODES):
-    print(X_ARRAY[I],Y_ARRAY[I],Q_ARRAY[I])
+    print(X_ARRAY[I],Y_ARRAY[I],THETA_ARRAY[I])
 
 #X,Y = NP.meshgrid(X_ARRAY,Y_ARRAY)
 X_ARRAY_MIN = NP.min(X_ARRAY)
 X_ARRAY_MAX = NP.max(X_ARRAY)
 Y_ARRAY_MIN = NP.min(Y_ARRAY)
 Y_ARRAY_MAX = NP.max(Y_ARRAY)
-print("X_ARRAY_MIN","X_ARRAY_MAX","Y_ARRAY_MIN","Y_ARRAY_MAX")
-print(X_ARRAY_MIN,X_ARRAY_MAX,Y_ARRAY_MIN,Y_ARRAY_MAX)
+#print("X_ARRAY_MIN","X_ARRAY_MAX","Y_ARRAY_MIN","Y_ARRAY_MAX")
+#print(X_ARRAY_MIN,X_ARRAY_MAX,Y_ARRAY_MIN,Y_ARRAY_MAX)
 
 
-FIGURE, AX = PLT.subplots(1,1,sharex=True,sharey=True) #(1,1,sharex=True,sharey=True)
-AX.set_aspect('equal')
-
-#Q_FIGURE_TIMESTEP = AX.tripcolor(X_ARRAY.flatten(),Y_ARRAY.flatten(),Q_ARRAY_DATE)
+FIGURE, AXIS = PLT.subplots(1,1,sharex=True,sharey=True) #(1,1,sharex=True,sharey=True)
+AXIS.set_aspect('equal')
 
 X_GRID = NP.linspace(X_ARRAY_MIN,X_ARRAY_MAX,int((X_ARRAY_MAX-X_ARRAY_MIN)/X_Y_STEP_SIZE))
 Y_GRID = NP.linspace(Y_ARRAY_MIN,Y_ARRAY_MAX,int((Y_ARRAY_MAX-Y_ARRAY_MIN)/X_Y_STEP_SIZE))
@@ -69,10 +58,22 @@ X_GRID,Y_GRID = NP.meshgrid(X_GRID,Y_GRID, indexing='xy')
 X_ARRAY_FLATTENED = X_ARRAY.ravel()
 Y_ARRAY_FLATTENED = Y_ARRAY.ravel()
 
-Q_RESAMPLED = scipy.interpolate.griddata((X_ARRAY.ravel(),Y_ARRAY.ravel()),Q_ARRAY_DATE.ravel(),(X_GRID,Y_GRID))
+#for J in range(0,NUM_DAYS):
+IMAGE_COLLECTION = []
+
+THETA_ARRAY = FIELDS_PD[["thNew"]].to_numpy()
+THETA_ARRAY_DATE = NP.zeros(NUM_NODES,dtype=float)  
+
+J = 153
+#for J in range(0,NUM_DAYS):
+for I in range(0,NUM_NODES):
+    THETA_ARRAY_DATE[I] = THETA_ARRAY[I+J*NUM_NODES]
+#    os.system("pause")
+
+THETA_RESAMPLED = scipy.interpolate.griddata((X_ARRAY.ravel(),Y_ARRAY.ravel()),THETA_ARRAY_DATE.ravel(),(X_GRID,Y_GRID))
 #X_RESAMPLED = NP.mgrid()
 
-Q_FIGURE_TIMESTEP = PLT.imshow(Q_RESAMPLED,
+THETA_FIGURE_TIMESTEP = PLT.imshow(THETA_RESAMPLED,
                                cmap='jet', 
                                interpolation='bilinear',    #bilinear   # nearest
                                origin='lower',
@@ -85,4 +86,4 @@ PLT.show()
 print(X_ARRAY)
 #print(Y_ARRAY)
 #X,Y = NP.meshgrid(X_ARRAY,Y_ARRAY)
-#PLT.tripcolor(X_ARRAY,Y_ARRAY,Q_ARRAY)
+#PLT.tripcolor(X_ARRAY,Y_ARRAY,THETA_ARRAY)
