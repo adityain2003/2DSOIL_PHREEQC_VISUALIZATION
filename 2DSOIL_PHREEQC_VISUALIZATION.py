92136fc368bbca58118d6a04ce09db5370d1b8e2
adityain2003@gmail.com <adityain2003@gmail.com>
2024-12-05 13:46:50 -0500

    WOKING FOR A TIME STEP

--- 2DSOIL_PHREEQC_VISUALIZATION.py
+++ 2DSOIL_PHREEQC_VISUALIZATION.py
@@ -3,12 +3,15 @@
 import matplotlib.pyplot as PLT
 import numpy as NP
 import pandas as PD
+import scipy.interpolate
+import matplotlib.animation as animation
 import os
 
 # FILE = open("TEST_FILE.txt", "r" )
 # FIELDS = FILE.read()
 # print(FIELDS)
 NUM_NODES = 600  # NUMBER OF NODES IN 2DSOIL
+X_Y_STEP_SIZE = 0.1   # RESOLUTION OF RESAMPLING GRID
 FIELDS_PD = PD.read_csv('TEST_FILE.txt',sep = ' *, *')
 print("READ FROM PANDAS")
 print(FIELDS_PD)
@@ -29,22 +32,54 @@ print("NUM_DAYS =", NUM_DAYS)
 X_ARRAY = (FIELDS_PD[["X"]].to_numpy())[0:NUM_NODES]
 Y_ARRAY = (FIELDS_PD[["Y"]].to_numpy())[0:NUM_NODES]
 
-Q_ARRAY = FIELDS_PD[["Q"]].to_numpy()
+Q_ARRAY = FIELDS_PD[["thNew"]].to_numpy()
 Q_ARRAY_DATE = NP.zeros(NUM_NODES,dtype=float)
 
 #for J in range(0,NUM_DAYS):
 
-J = 0
-for J in range(0,NUM_DAYS):
-    for I in range(0,NUM_NODES):
-        Q_ARRAY_DATE[I] = Q_ARRAY[I+J*NUM_NODES]
-        os.system("pause")
+J = 153
+#for J in range(0,NUM_DAYS):
+for I in range(0,NUM_NODES):
+    Q_ARRAY_DATE[I] = Q_ARRAY[I+J*NUM_NODES]
+#    os.system("pause")
         
 
 for I in range(0,NUM_NODES):
-    print(X_ARRAY[I],Y_ARRAY[I],Q_ARRAY[1])
+    print(X_ARRAY[I],Y_ARRAY[I],Q_ARRAY[I])
+
+#X,Y = NP.meshgrid(X_ARRAY,Y_ARRAY)
+X_ARRAY_MIN = NP.min(X_ARRAY)
+X_ARRAY_MAX = NP.max(X_ARRAY)
+Y_ARRAY_MIN = NP.min(Y_ARRAY)
+Y_ARRAY_MAX = NP.max(Y_ARRAY)
+print("X_ARRAY_MIN","X_ARRAY_MAX","Y_ARRAY_MIN","Y_ARRAY_MAX")
+print(X_ARRAY_MIN,X_ARRAY_MAX,Y_ARRAY_MIN,Y_ARRAY_MAX)
 
 
+FIGURE, AX = PLT.subplots(1,1,sharex=True,sharey=True) #(1,1,sharex=True,sharey=True)
+AX.set_aspect('equal')
+
+#Q_FIGURE_TIMESTEP = AX.tripcolor(X_ARRAY.flatten(),Y_ARRAY.flatten(),Q_ARRAY_DATE)
+
+X_GRID = NP.linspace(X_ARRAY_MIN,X_ARRAY_MAX,int((X_ARRAY_MAX-X_ARRAY_MIN)/X_Y_STEP_SIZE))
+Y_GRID = NP.linspace(Y_ARRAY_MIN,Y_ARRAY_MAX,int((Y_ARRAY_MAX-Y_ARRAY_MIN)/X_Y_STEP_SIZE))
+
+X_GRID,Y_GRID = NP.meshgrid(X_GRID,Y_GRID, indexing='xy')
+
+X_ARRAY_FLATTENED = X_ARRAY.ravel()
+Y_ARRAY_FLATTENED = Y_ARRAY.ravel()
+
+Q_RESAMPLED = scipy.interpolate.griddata((X_ARRAY.ravel(),Y_ARRAY.ravel()),Q_ARRAY_DATE.ravel(),(X_GRID,Y_GRID))
+#X_RESAMPLED = NP.mgrid()
+
+Q_FIGURE_TIMESTEP = PLT.imshow(Q_RESAMPLED,
+                               cmap='jet', 
+                               interpolation='bilinear',    #bilinear   # nearest
+                               origin='lower',
+                               extent=[X_ARRAY_MIN,X_ARRAY_MAX,Y_ARRAY_MIN,Y_ARRAY_MAX],)
+
+PLT.colorbar()
+PLT.show()
 
 #print(UNIQUE_DATES)
 print(X_ARRAY)
